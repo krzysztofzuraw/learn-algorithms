5a1f5e06c49c2edbf0119f7509f0effcd1c129d4
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-04-01 18:18:24 +0200

    Basic of Linked Lists

---
+++ linked_lists/analysis.md
@@ -0,0 +1,26 @@
+Linked lists
+============
+
+This is data structure that take advantage of references. Linked lists are made up of nodes. Each
+node contains reference to the next node in the list. What is more, each node contains a data.
+
+A linked list is:
+
+* the empty lists, represented by None
+* a node that contains data object and a reference to a linked list
+
+# Pseudocode
+
+```
+structure node:
+  next_node = none
+  data = none
+  
+structure linked_list:
+  length = 0
+  head = none
+```
+
+# References
+
+* [Linked lists](http://greenteapress.com/thinkpython/html/chap17.html)
---
+++ linked_lists/linked_lists.py
@@ -0,0 +1,58 @@
+class Node(object):
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    def __repr__(self):
+        return 'Node with {} data'.format(self.data)
+
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.length = 0
+        self.head = head
+
+    def __iter__(self):
+        self.count_length(self.head)
+        return self
+
+    def count_length(self, head):
+        while head:
+            self.length += 1
+            return self.count_length(head.next_node)
+
+    def __next__(self):
+        if self.length <= 0:
+            raise StopIteration
+        self.length -= 1
+        return self.head
+
+
+def test_node_is_empty():
+    node = Node()
+    assert node.data is None
+    assert node.next_node is None
+
+
+def test_node_have_data():
+    node = Node(data='data')
+    assert node.data == 'data'
+
+
+def test_node_have_reference_to_another_node():
+    second_node = Node()
+    first_node = Node(next_node=second_node)
+    assert first_node.next_node == second_node
+
+
+def test_linked_list_with_one_node():
+    node = Node()
+    linked_list = LinkedList(head=node)
+    assert [item for item in linked_list] == [node]
+
+# def test_linked_list_with_two_nodes():
+#     first_node = Node(data='first_node')
+#     second_node = Node(data='second_node')
+#     first_node.next_node = second_node
+#     linked_list = LinkedList(head=first_node)
+#     assert [item for item in linked_list] == [first_node, second_node]
