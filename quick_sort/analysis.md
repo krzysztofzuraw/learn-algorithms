8157a9589fbc471cbe533b0f9e40dc586101fc77
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-12-24 11:40:59 +0100

    Add basic implementation of quick sort

--- quick_sort/analysis.md
+++ quick_sort/analysis.md
@@ -1,5 +1,21 @@
 # Quick Sort
 
+
+```
+def partition(a):
+    p,q,j = 0
+    r = len(a) - 1
+    while j != r:
+        if a[j] > a[r]:
+            j++
+        elif a[j] =< a[r]:
+            swap a[j] and a[r]
+            j++
+            q++
+    swap a[r] and a[q]
+    return a,q
+```
+
 ## Complexity
 
 ### References
--- quick_sort/quick_sort.py
+++ quick_sort/quick_sort.py
@@ -1,2 +1,31 @@
+def partition(a):
+    q = 0
+    j = 0
+    r = len(a) - 1
+    while j < r:
+        if a[j] > a[r]:
+            j += 1
+        elif a[j] <= a[r]:
+            a[j], a[q] = a[q], a[j]
+            j += 1
+            q += 1
+    a[r], a[q] = a[q], a[r]
+    return a, q
+
+
+def quick_sort(a):
+    a, q = partition(a)
+    return quick_sort(a[0: q])
+    return quick_sort(a[q + 1: len(a) - 1])
+
+
 def test_partition():
     assert partition([12, 7, 14, 9, 10, 11]) == ([7, 9, 10, 11, 14, 12], 3)
+
+
+def test_partition_more_complex():
+    assert partition([9, 7, 5, 11, 12, 2, 14, 3, 10, 6]) == ([5, 2, 3, 6, 12, 7, 14, 9, 10, 11], 3)
+
+
+def test_quicksort():
+    assert quick_sort([12, 7, 14, 9, 10, 11]) == [7, 9, 10, 11, 12, 14]
