21c219456fc765e040b04f2a83a2f13b86e8efd0
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-12-29 11:49:04 +0100

    Added is_leaf

--- tree/tree.py
+++ tree/tree.py
@@ -5,10 +5,12 @@ class Node:
         self.right_child = right_child
         self.value = value
 
-    def get_children(self):
+    @property
+    def children(self):
         return self.left_child, self.right_child
 
-    def get_sibling(self):
+    @property
+    def sibling(self):
         if self.parent:
             if self.parent.left_child == self:
                 return self.parent.right_child
@@ -16,6 +18,14 @@ class Node:
                 return self.parent.left_child
 
         return "No parent node"
+
+    @property
+    def is_leaf(self):
+        return False if self.left_child or self.right_child else True
+
+    @property
+    def degree(self):
+        return self.left_child + self.right_child
 
 
 def test_root_node():
@@ -33,7 +43,7 @@ def test_root_node():
     assert root_node.right_child == right_child_node
 
 
-def test_get_children():
+def test_children():
     """
             (2)
              |
@@ -42,10 +52,10 @@ def test_get_children():
     root_node = Node(value=2)
     left_child_node = Node(parent=root_node, value=5)
     root_node.left_child = left_child_node
-    assert root_node.get_children() == (left_child_node, None)
+    assert root_node.children == (left_child_node, None)
 
 
-def test_get_siblings():
+def test_sibling():
     """
             (2)
            /  \
@@ -56,5 +66,33 @@ def test_get_siblings():
     right_child_node = Node(parent=root_node, value=7)
     root_node.left_child = left_child_node
     root_node.right_child = right_child_node
-    assert left_child_node.get_sibling() == right_child_node
-    assert right_child_node.get_sibling() == left_child_node
+    assert left_child_node.sibling == right_child_node
+    assert right_child_node.sibling == left_child_node
+
+
+def test_leaf():
+    """
+        (1)
+         |
+        (2)
+    """
+    root_node = Node(value=1)
+    left_child_node = Node(parent=root_node, value=2)
+    root_node.left_child = left_child_node
+    assert left_child_node.is_leaf is True
+    assert root_node.is_leaf is False
+
+
+def test_degree():
+    """
+        (1)
+       /   \
+     (2)   (3)
+    """
+    root_node = Node(value=1)
+    left_child_node = Node(parent=root_node, value=2)
+    right_child_node = Node(parent=root_node, value=3)
+    root_node.left_child = left_child_node
+    root_node.right_child = right_child_node
+    assert root_node.degree == 2
+    assert left_child_node.degree == 0
