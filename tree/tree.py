ea7ad7dd98fa9d1b844c7e310d1fe879d1f96f75
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2018-01-04 20:12:21 +0100

    Fix egde method & add fixture

--- tree/tree.py
+++ tree/tree.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class Node:
     def __init__(self, parent=None, left_child=None, right_child=None, value=None):
         self.parent = parent
@@ -39,10 +42,13 @@ class Tree:
     def __init__(self, list_of_nodes=None):
         self.list_of_nodes = list_of_nodes
 
-    def edge(self, start, end):
+    @staticmethod
+    def edge(start, end):
+        if not start or not end:
+            return False
         if start.left_child == end or start.right_child == end:
             return True
-        elif end.children[0] == start or end.children[1] == start:
+        elif end.left_child == start or end.right_child == start:
             return True
         return False
 
@@ -56,6 +62,47 @@ class Tree:
             yield from self.path(start.left_child, end)
         elif self.edge(start, start.right_child):
             yield from self.path(start.right_child, end)
+
+
+@pytest.fixture
+def create_tree():
+    """
+            (2)
+           /   \
+         (7)   (5)
+        /  \     \
+      (2)  (6)   (9)
+           / \   /
+        (5)(11) (4)
+    """
+    root_node = Node(value=2)
+    seventh_node = Node(parent=root_node, value=7)
+    fifth_node = Node(parent=root_node, value=5)
+    second_node = Node(parent=seventh_node, value=2)
+    sixth_node = Node(parent=seventh_node, value=6)
+    nineth_node = Node(parent=fifth_node, value=9)
+    second_fifth_node = Node(parent=sixth_node, value=5)
+    eleventh_node = Node(parent=sixth_node, value=11)
+    fourth_node = Node(parent=nineth_node, value=4)
+
+    root_node.left_child = seventh_node
+    root_node.right_child = fifth_node
+
+    seventh_node.left_child = second_node
+    seventh_node.right_child = sixth_node
+
+    fifth_node.right_child = nineth_node
+
+    sixth_node.right_child = second_fifth_node
+    sixth_node.left_child = eleventh_node
+
+    nineth_node.right_child = fourth_node
+
+    return Tree([
+        root_node, seventh_node, fifth_node,
+        second_node, sixth_node, nineth_node,
+        second_fifth_node, eleventh_node, fourth_node
+    ])
 
 
 def test_root_node():
@@ -170,6 +217,15 @@ def test_edge_more_complex():
     assert tree.edge(root_node, right_child_node) is True
 
 
+def test_edge_non_existing():
+    """
+        (1)
+    """
+    root_node = Node(value=1)
+    tree = Tree([root_node])
+    assert tree.edge(root_node, None) is False
+
+
 def test_path_for_left_child():
     """
         (1)
@@ -212,3 +268,20 @@ def test_path_for_right_child():
     tree = Tree([root_node, child_node, last_node, four_node])
     assert [i for i in tree.path(root_node, last_node)] == [root_node, child_node, last_node]
     assert [i for i in tree.path(root_node, four_node)] == [root_node, child_node, last_node, four_node]
+
+
+def test_path_for_both_left_and_right():
+    """
+        (1)
+        /
+       (2)
+         \
+         (3)
+    """
+    root_node = Node(value=1)
+    second_node = Node(parent=root_node, value=2)
+    third_node = Node(parent=second_node, value=3)
+    root_node.left_child = second_node
+    second_node.right_child = third_node
+    tree = Tree([root_node, second_node, third_node])
+    assert [i for i in tree.path(root_node, third_node)] == [root_node, second_node, third_node]
